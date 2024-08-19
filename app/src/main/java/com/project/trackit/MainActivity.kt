7deb14bd8ea6020d6368b8a2ce15df75d9463963
EstradaJesus19 <geifa@uninorte.edu.co>
2024-08-19 12:59:17 -0500

    Nueva versión

--- app/src/main/java/com/project/trackit/MainActivity.kt
+++ app/src/main/java/com/project/trackit/MainActivity.kt
@@ -6,19 +6,19 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.net.DatagramPacket
+import java.net.InetAddress
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 import java.util.*
-
+import java.net.Socket
+import java.net.DatagramSocket
 
 class MainActivity : AppCompatActivity(), LocationListener {
 
@@ -26,97 +26,153 @@ class MainActivity : AppCompatActivity(), LocationListener {
     private lateinit var longitudeText: TextView
     private lateinit var latitudeText: TextView
     private lateinit var dateTimeText: TextView
-    private lateinit var phoneNumber: EditText
-    private lateinit var sendButton: Button
+    private lateinit var sendButton_UDP: Button
+    private lateinit var sendButton_TCP: Button
 
     private var currentData: String = ""
+    private var bestLocation: Location? = null
 
     companion object {
-        const val SMS_PERMISSION_CODE = 100
         const val LOCATION_PERMISSION_CODE = 101
-    }
-
-    // Handling location updates
-    override fun onLocationChanged(location: Location) {
-        val lat = location.latitude
-        val lon = location.longitude
-        val dateTimeFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-        dateTimeFormat.timeZone = TimeZone.getDefault()
-        val localDateTime = dateTimeFormat.format(Date(location.time))
-        currentData = "Lat: $lat, \nLon: $lon \nDate/Time: $localDateTime"
-        latitudeText.text = "$lat"
-        longitudeText.text = "$lon"
-        dateTimeText.text = "$localDateTime"
+        const val TCP_PORT = 60000
+        const val UDP_PORT = 60001
+        const val IP_ADDRESS_1 = "152.204.170.240"  // IP 1
+        const val IP_ADDRESS_2 = "161.10.95.122"  // IP 2
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initializing interface components
         latitudeText = findViewById(R.id.latitudeValue)
         longitudeText = findViewById(R.id.longitudeValue)
         dateTimeText = findViewById(R.id.dateTimeValue)
-        phoneNumber = findViewById(R.id.phoneNumber)
-        sendButton = findViewById(R.id.sendButton)
+        sendButton_UDP = findViewById(R.id.sendButton_UDP)
+        sendButton_TCP = findViewById(R.id.sendButton_TCP)
 
-        // Initializing LocationManager
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
 
-        // Check and request location permissions if necessary
         if (!checkLocationPermissions()) {
             requestLocationPermissions()
         } else {
             startLocationUpdates()
         }
 
-        // Check and request SMS permissions if necessary
-        if (!checkSmsPermission()) {
-            requestSmsPermission()
-        }
-
-        // Action when pressing the send button
-        sendButton.setOnClickListener {
-            val phone = phoneNumber.text.toString()
-
-            if (phone.length < 10) {
-                Toast.makeText(this, "Invalid phone number", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            Log.d("PhoneNumber", "Number entered: $phone")
-
-            if (phone.isNotEmpty()) {
-                // Build message with current location and time
-                val message = "$currentData"
-                if (checkSmsPermission()) {
-                    sendSMS(phone, message)
-                } else {
-                    Toast.makeText(this, "Permission to send SMS not granted", Toast.LENGTH_SHORT).show()
-                    requestSmsPermission()
-                }
+        sendButton_UDP.setOnClickListener {
+            if (currentData.isNotEmpty()) {
+                sendLocationData_UDP(IP_ADDRESS_1)
+                sendLocationData_UDP(IP_ADDRESS_2)
+                Toast.makeText(this, "UDP: Data sent", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "Please enter a phone number", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "No location data available", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        sendButton_TCP.setOnClickListener {
+            if (currentData.isNotEmpty()) {
+                sendLocationData_TCP(IP_ADDRESS_1)
+                sendLocationData_TCP(IP_ADDRESS_2)
+                Toast.makeText(this, "TCP: Data sent", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "No location data available", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
+    override fun onLocationChanged(location: Location) {
+        if (isBetterLocation(location, bestLocation)) {
+            bestLocation = location
+            val lat = location.latitude
+            val lon = location.longitude
+            val dateTimeFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            dateTimeFormat.timeZone = TimeZone.getDefault()
+            val localDateTime = dateTimeFormat.format(Date(location.time))
+            currentData = "Lat: $lat, Lon: $lon, Date/Time: $localDateTime"
 
-    // Send SMS with location and time
-    private fun sendSMS(phoneNumber: String, message: String) {
-        try {
-            Log.d("SMS", "Sending SMS to the phone number: $phoneNumber")
-            val smsManager = SmsManager.getDefault()
-            smsManager.sendTextMessage(phoneNumber, null, message, null, null)
-            Log.d("SMS", "SMS sent successfully")
-            Toast.makeText(this, "SMS sent successfully", Toast.LENGTH_SHORT).show()
-        } catch (e: Exception) {
-            Log.e("SMS", "Error sending SMS: ${e.message}")
-            Toast.makeText(this, "Error sending SMS: ${e.message}", Toast.LENGTH_LONG).show()
+            // Update UI with current location
+            latitudeText.text = "$lat"
+            longitudeText.text = "$lon"
+            dateTimeText.text = "$localDateTime"
+
+            Log.d("LocationUpdate", "Provider: ${location.provider}, Data: $currentData")
         }
     }
 
-    // Resulting permit request
+    private fun isBetterLocation(newLocation: Location, currentBestLocation: Location?): Boolean {
+        if (currentBestLocation == null) {
+            return true
+        }
+
+        // Checking newest location
+        val timeDelta = newLocation.time - currentBestLocation.time
+        val isSignificantlyNewer = timeDelta > 120000  // 2 minutos
+        val isSignificantlyOlder = timeDelta < -120000  // 2 minutos
+        val isNewer = timeDelta > 0
+
+        if (isSignificantlyNewer) {
+            return true
+        } else if (isSignificantlyOlder) {
+            return false
+        }
+
+        // Checking precision
+        val accuracyDelta = (newLocation.accuracy - currentBestLocation.accuracy).toInt()
+        val isMoreAccurate = accuracyDelta < 0
+        val isLessAccurate = accuracyDelta > 0
+
+        // Checking provider
+        val isFromSameProvider = newLocation.provider == currentBestLocation.provider
+
+        // Best location determination
+        return when {
+            isMoreAccurate -> true
+            isNewer && !isLessAccurate -> true
+            isNewer && !isFromSameProvider -> true
+            else -> false
+        }
+    }
+
+    private fun sendLocationData_UDP(ipAddress: String) {
+        Thread {
+            try {
+                // Send UDP
+                val socket_udp = DatagramSocket()
+                val address = InetAddress.getByName(ipAddress)
+                val message_udp = currentData.toByteArray()
+                val packet = DatagramPacket(message_udp, message_udp.size, address, UDP_PORT)
+                socket_udp.send(packet)
+                socket_udp.close()
+                Log.d("UDP", "Data sent to $ipAddress")
+            } catch (e: Exception) {
+                Log.e("UDP", "Error sending data to $ipAddress: ${e.message}")
+                runOnUiThread {
+                    Toast.makeText(this, "Error sending data: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+        }.start()
+    }
+
+    private fun sendLocationData_TCP(ipAddress: String) {
+        Thread {
+            try {
+                // SEND TCP
+                val socket_tcp = Socket(ipAddress, TCP_PORT)
+                val outputStream = socket_tcp.getOutputStream()
+                val message_tcp = currentData.toByteArray()
+                outputStream.write(message_tcp)
+                outputStream.flush()
+                outputStream.close()
+                socket_tcp.close()
+                Log.d("TCP", "Data sent to $ipAddress")
+            } catch (e: Exception) {
+                Log.e("TCP", "Error sending data to $ipAddress: ${e.message}")
+                runOnUiThread {
+                    Toast.makeText(this, "Error sending data: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+        }.start()
+    }
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when (requestCode) {
             LOCATION_PERMISSION_CODE -> {
@@ -127,50 +183,31 @@ class MainActivity : AppCompatActivity(), LocationListener {
                     Toast.makeText(this, "Location permission denied.", Toast.LENGTH_SHORT).show()
                 }
             }
-            SMS_PERMISSION_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "SMS permission granted.", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "SMS permission denied.", Toast.LENGTH_SHORT).show()
-                }
-            }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    // Check if location permissions are granted
     private fun checkLocationPermissions(): Boolean {
         val fineLocation = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
         val coarseLocation = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
         return fineLocation == PackageManager.PERMISSION_GRANTED && coarseLocation == PackageManager.PERMISSION_GRANTED
     }
 
-    // Request location permissions
     private fun requestLocationPermissions() {
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSION_CODE)
     }
 
-    // Check if permission to send SMS is granted
-    private fun checkSmsPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED
-    }
-
-    // Request permission to send SMS
-    private fun requestSmsPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), SMS_PERMISSION_CODE)
-    }
-
-    // Start location updates
     private fun startLocationUpdates() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            // Solicitar actualizaciones de ambos proveedores
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, this)
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, this)
         }
     }
 
     override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
     override fun onProviderEnabled(provider: String) {}
     override fun onProviderDisabled(provider: String) {}
-
-
 }
